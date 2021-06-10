402aa1cc378ed1babd8cd16bc36e21048f5a41e2
Tanner Legasse <tanner.legasse@gmail.com>
2021-06-10 17:23:53 -0400

    Another commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import './App.css';
-import Button from '@material-ui/core/Button';
-import Link from '@material-ui/core/Link';
-import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
-import { withStyles } from '@material-ui/core/styles'
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import FormatQuoteIcon from "@material-ui/icons/FormatQuote";
+import { withStyles } from "@material-ui/core/styles";
 
-
-    
 const styles = (theme) => ({
   root: {
     flexGrow: 1,
@@ -28,53 +26,62 @@ const styles = (theme) => ({
     },
     "& .quote-author": {
       textAlign: "right",
-      display: "block"
+      display: "block",
     },
     "& #new-quote": {
-      minWidth: "125px"
+      minWidth: "125px",
     },
     "& .submit-container": {
-      textAlign: "right"
-    }
-  }
+      textAlign: "right",
+    },
+  },
 });
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
-    
+    super(props);
+
     this.state = {
       quote: {
         text: "Please Wait...",
-        author: "Please Wait..."
+        author: "Please Wait...",
       },
-      tweetLink: '#'
-    }
+      tweetLink: "#",
+    };
 
-    fetch('https://type.fit/api/quotes')
+    fetch("https://type.fit/api/quotes")
       .then((res) => res.json())
-      .then(quotes => {
-        let chosenQuote = quotes[Math.floor(Math.random() * quotes.length)]
+      .then((quotes) => {
+        let chosenQuote = quotes[Math.floor(Math.random() * quotes.length)];
         this.setState({
           quotes: quotes,
           quote: chosenQuote,
-          tweetLink: this.getTweetLink(chosenQuote)
-        })
-      })
+          tweetLink: this.getTweetLink(chosenQuote),
+        });
+      });
     this.getNewQuote = this.getNewQuote.bind(this);
+
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
+
+    document.head.appendChild(s);
   }
 
   getNewQuote() {
-    let chosenQuote = this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)]
+    let chosenQuote =
+      this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)];
     this.setState({
       quote: chosenQuote,
-      tweetLink: this.getTweetLink(chosenQuote)
-    })
+      tweetLink: this.getTweetLink(chosenQuote),
+    });
   }
 
   getTweetLink(chosenQuote) {
-    let tweetLinkPrefix = 'https://twitter.com/intent/tweet?'
-    let linkBody = encodeURIComponent(chosenQuote.text + ' - ' + chosenQuote.author)
+    let tweetLinkPrefix = "https://twitter.com/intent/tweet?";
+    let linkBody = encodeURIComponent(
+      chosenQuote.text + " - " + chosenQuote.author
+    );
     return tweetLinkPrefix + linkBody;
   }
 
@@ -86,18 +93,32 @@ class App extends React.Component {
           <Grid item xs={12} sm={3}>
             <Card>
               <CardContent>
-                <FormatQuoteIcon />{this.state.quote.text}
-                
-                <Typography className="quote-author" gutterBottom variant="h5" component="caption">
+                <FormatQuoteIcon />
+                {this.state.quote.text}
+
+                <Typography
+                  className="quote-author"
+                  gutterBottom
+                  variant="h5"
+                  component="caption"
+                >
                   - {this.state.quote.author}
                 </Typography>
-                
+
                 <Grid className="actions-row">
                   <Grid item xs={12} sm={6} className="social-container">
-                    <Link href={this.state.tweetLink} id="tweet-quote"><TwitterIcon /></Link>
+                    <Link href={this.state.tweetLink} id="tweet-quote">
+                      <TwitterIcon />
+                    </Link>
                   </Grid>
                   <Grid className="submit-container" item xs={12} sm={6}>
-                    <Button id="new-quote" onClick={this.getNewQuote} variant="contained">New Quote</Button>
+                    <Button
+                      id="new-quote"
+                      onClick={this.getNewQuote}
+                      variant="contained"
+                    >
+                      New Quote
+                    </Button>
                   </Grid>
                 </Grid>
               </CardContent>
